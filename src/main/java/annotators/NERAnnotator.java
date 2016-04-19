654ba473153edcc2a4ee55bd5aa505812ed1ecf8
Colin Graber <cgraber2@illinois.edu>
2016-04-18 20:02:30 -0500

    Added a bit of documentation to the Annotator

--- src/main/java/annotators/NERAnnotator.java
+++ src/main/java/annotators/NERAnnotator.java
@@ -22,10 +22,17 @@ public class NERAnnotator extends Annotator {
 
     @Override
     public void addView(TextAnnotation ta) throws AnnotatorException {
+        //When writing an annotator for our system, there are two main steps:
+        //First, you need to make an ACEAnnotation from the text annotation. There's a constructor to do this:
         ACEAnnotation doc = new ACEAnnotation(ta);
+
+        //All you have to do now is run your test code on this ACEAnnotation
         NERBaseline ner = new NERBaseline();
         List<ACEAnnotation> arg = new ArrayList<>();
         arg.add(doc);
         ner.test(arg);
+
+        //As long as your code adds the NER, coreference, or relation information to the ACEAnnotation document, the
+        //correct information will be automatically added to the TextAnnotation!
     }
 }
